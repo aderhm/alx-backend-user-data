bdb5de0a9d3bccc2b8ab684abfa5abe28526e9ad
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-13 07:37:08 +0100

    Implement update_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -58,16 +58,12 @@ class DB:
     def update_user(self, id: int, **kwargs) -> None:
         """ Update user
         """
-        if id:
-            user = self.find_user_by(id=id)
-            if not user:
-                raise NoResultFound
+        user = self.find_user_by(id=id)
 
-            user_attributes = User.__table__.columns.keys()
-            for k in kwargs:
-                if k not in user_attributes:
-                    raise InvalidRequestError
+        user_attributes = User.__table__.columns.keys()
+        for k, v in kwargs.items():
+            if k not in user_attributes:
+                raise ValueError
+            setattr(user, k, v)
 
-            for k, v in kwargs.items():
-                setattr(user, k, v)
-            self._session.commit()
+        self._session.commit()
