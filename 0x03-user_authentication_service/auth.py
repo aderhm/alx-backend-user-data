0a15b9c6c7cc297d1c506339f1f516ab295d5755
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-14 12:21:54 +0100

    Create session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,14 @@ class Auth:
             return bcrypt.checkpw(pw_bytes, existing_user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Creates a session for a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            sid = _generate_uuid()
+            self._db.update_user(user.id, session_id=sid)
+            return sid
+        except NoResultFound:
+            return None
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -91,6 +91,20 @@ Main file
 # except ValueError as err:
 #     print("could not create a new user: {}".format(err))
 
+# from auth import Auth
+
+# email = 'bob@bob.com'
+# password = 'MyPwdOfBob'
+# auth = Auth()
+
+# auth.register_user(email, password)
+
+# print(auth.valid_login(email, password))
+
+# print(auth.valid_login(email, "WrongPwd"))
+
+# print(auth.valid_login("unknown@email", password))
+
 from auth import Auth
 
 email = 'bob@bob.com'
@@ -99,8 +113,5 @@ auth = Auth()
 
 auth.register_user(email, password)
 
-print(auth.valid_login(email, password))
-
-print(auth.valid_login(email, "WrongPwd"))
-
-print(auth.valid_login("unknown@email", password))
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
