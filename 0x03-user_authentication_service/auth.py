2eb9fcac7b4421d155ec9b16c7e0a83ee8708726
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-14 13:46:42 +0100

    Find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,3 +61,13 @@ class Auth:
             return sid
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Find user by session id
+        """
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
