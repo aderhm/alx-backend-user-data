95603ee656f4f90b7c955546b796dd7fde1cda6e
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-14 11:20:09 +0100

    Implement a method to validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,13 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate credentials
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            pw_bytes = password.encode('utf-8')
+            return bcrypt.checkpw(pw_bytes, existing_user.hashed_password)
+        except NoResultFound:
+            return False
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -72,21 +72,35 @@ Main file
 
 # print(_hash_password("Hello Holberton"))
 
+# from auth import Auth
+
+# email = 'me@me.com'
+# password = 'mySecuredPwd'
+
+# auth = Auth()
+
+# try:
+#     user = auth.register_user(email, password)
+#     print("successfully created a new user!")
+# except ValueError as err:
+#     print("could not create a new user: {}".format(err))
+
+# try:
+#     user = auth.register_user(email, password)
+#     print("successfully created a new user!")
+# except ValueError as err:
+#     print("could not create a new user: {}".format(err))
+
 from auth import Auth
 
-email = 'me@me.com'
-password = 'mySecuredPwd'
-
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
 auth = Auth()
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+auth.register_user(email, password)
 
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+print(auth.valid_login(email, password))
+
+print(auth.valid_login(email, "WrongPwd"))
+
+print(auth.valid_login("unknown@email", password))
