939a2e6a29fb2001196ad8d0c5bba9a856f4f6f9
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-15 11:48:09 +0100

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,17 @@ class Auth:
             return rtoken
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user.id,
+                hashed_password=hashed_password
+                reset_token=reset_token
+                )
+        except NoResultFound:
+            raise ValueError
