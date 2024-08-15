bc9c191152357c5728317e14de25f0f5e2341cd3
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-15 11:52:05 +0100

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,7 +97,7 @@ class Auth:
             hashed_password = _hash_password(password)
             self._db.update_user(
                 user.id,
-                hashed_password=hashed_password
+                hashed_password=hashed_password,
                 reset_token=reset_token
                 )
         except NoResultFound:
